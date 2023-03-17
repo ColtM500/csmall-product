b9c27845142188698a8643cb8c2869b1a1602818
chen <602042954@qq.com>
2023-03-17 14:42:07 +0800

    test00

--- src/test/java/cn/chen/csmall/product/CsmallProductApplicationTests.java
+++ src/test/java/cn/chen/csmall/product/CsmallProductApplicationTests.java
@@ -1,13 +1,24 @@
 package cn.chen.csmall.product;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class CsmallProductApplicationTests {
+    @Autowired  //在此处禁止随意添加required=false
+    DataSource dataSource;
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void getConnection() throws Throwable{
+        dataSource.getConnection();//调用此方法 会连接数据库
+    }
+
 
 }
