cd0705c6dc283c88597a96fcc6783d2a26c58bc4
chen <602042954@qq.com>
2023-03-20 20:07:32 +0800

    slf4

--- src/main/java/cn/chen/csmall/product/mapper/SkuSpecificationMapper.java
+++ src/main/java/cn/chen/csmall/product/mapper/SkuSpecificationMapper.java
@@ -1,9 +1,21 @@
 package cn.chen.csmall.product.mapper;
 
 import cn.chen.csmall.product.pojo.entity.SkuSpecification;
+import cn.chen.csmall.product.pojo.vo.SkuListItemVO;
+import cn.chen.csmall.product.pojo.vo.SkuSpecificationListItemVO;
+import cn.chen.csmall.product.pojo.vo.SkuSpecificationStandardVO;
+import cn.chen.csmall.product.pojo.vo.SkuStandardVO;
+
+import java.util.List;
 
 public interface SkuSpecificationMapper {
     int insert(SkuSpecification skuSpecification);
 
     int deleteById(long id);
+
+    int count();
+
+   SkuSpecificationStandardVO getStandardById(Long id);
+
+    List<SkuSpecificationListItemVO> list();
 }
