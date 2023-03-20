affbc6e0fc968f83061259f96c632b1b88261d31
chen <602042954@qq.com>
2023-03-20 19:53:45 +0800

    slf4

--- src/main/java/cn/chen/csmall/product/mapper/PictureMapper.java
+++ src/main/java/cn/chen/csmall/product/mapper/PictureMapper.java
@@ -1,9 +1,21 @@
 package cn.chen.csmall.product.mapper;
 
 import cn.chen.csmall.product.pojo.entity.Picture;
+import cn.chen.csmall.product.pojo.vo.CategoryListItemVO;
+import cn.chen.csmall.product.pojo.vo.CategoryStandardVO;
+import cn.chen.csmall.product.pojo.vo.PictureListItemVO;
+import cn.chen.csmall.product.pojo.vo.PictureStandardVO;
+
+import java.util.List;
 
 public interface PictureMapper {
     int insert(Picture picture);
 
     int deleteById(long id);
+
+    int count();
+
+    PictureStandardVO getStandardById(Long id);
+
+    List<PictureListItemVO> list();
 }
