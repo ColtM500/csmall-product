9641684adeefc376598eb2377b18bbdddaf6083d
chen <602042954@qq.com>
2023-03-20 19:34:37 +0800

    slf4

--- src/main/java/cn/chen/csmall/product/mapper/BrandCategoryMapper.java
+++ src/main/java/cn/chen/csmall/product/mapper/BrandCategoryMapper.java
@@ -1,9 +1,19 @@
 package cn.chen.csmall.product.mapper;
 
 import cn.chen.csmall.product.pojo.entity.BrandCategory;
+import cn.chen.csmall.product.pojo.vo.BrandListItemVO;
+import cn.chen.csmall.product.pojo.vo.BrandStandardVO;
+
+import java.util.List;
 
 public interface BrandCategoryMapper {
     int insert(BrandCategory brandCategory);
 
     int deleteById(long id);
+
+    int count();
+
+    BrandStandardVO getStandardById(Long id);
+
+    List<BrandListItemVO> list();
 }
---
+++ src/main/java/cn/chen/csmall/product/pojo/vo/BrandCategoryStandardVO.java
@@ -0,0 +1,25 @@
+package cn.chen.csmall.product.pojo.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class BrandCategoryStandardVO implements Serializable {
+    /**
+     * 数据id
+     */
+    private Long id;
+
+    /**
+     * 品牌id
+     */
+    private Long brandId;
+
+    /**
+     * 类别id
+     */
+    private Long categoryId;
+
+
+}
