0d01ae865e4c2ea036623d3ddf446bb3359d2ef2
chen <602042954@qq.com>
2023-03-20 15:04:55 +0800

    slf4

--- src/main/java/cn/chen/csmall/product/mapper/AlbumMapper.java
+++ src/main/java/cn/chen/csmall/product/mapper/AlbumMapper.java
@@ -3,9 +3,17 @@ package cn.chen.csmall.product.mapper;
 import cn.chen.csmall.product.pojo.entity.Album;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlbumMapper {
     int insert(Album album);
 
+    int insertBatch(List<Album> albumList);
+
     int deleteById(Long id);
+
+    int deleteByIds(Long[] idList);
+
+    int update(Album album);
 }
--- src/test/java/cn/chen/csmall/product/mapper/AlbumMapperTests.java
+++ src/test/java/cn/chen/csmall/product/mapper/AlbumMapperTests.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 public class AlbumMapperTests {
     @Autowired
@@ -24,7 +28,42 @@ public class AlbumMapperTests {
     }
 
     @Test
+    void insertBatch(){
+        LocalDateTime now = LocalDateTime.now();
+        List<Album> albumList = new ArrayList<>();
+        for (int i=1; i<=5 ;i++){
+            Album album = new Album();
+            album.setName("测试相册" + i);
+            album.setDescription("测试相册00" + i + "的简介");
+            album.setSort(90 + i);
+            album.setGmtCreate(now);
+            album.setGmtModified(now);
+            albumList.add(album);
+        }
+        int rows =  mapper.insertBatch(albumList);
+        System.out.println("批量插入数据完成, 受影响的行数为: " + rows);
+    }
+
+    @Test
     void deleteById(){
         mapper.deleteById(11L);
+    }
+
+    @Test
+    void deleteByIds(){
+        Long[] idList = {13L, 14L, 15L, 16L, 17L};
+
+        int rows = mapper.deleteByIds(idList);
+        System.out.println("受影响行数为: " + rows);
+    }
+
+    @Test
+    void update(){
+        Album album = new Album();
+        album.setId(5L);
+        album.setName("新-测试数据005");
+
+        int rows = mapper.update(album);
+        System.out.println("受影响行数: "+rows);
     }
 }
