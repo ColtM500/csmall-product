aa313de57fa79f3df71004bff38c5cb786d9941f
chen <602042954@qq.com>
2023-03-20 20:28:02 +0800

    slf4

--- src/main/java/cn/chen/csmall/product/mapper/SpuDetailMapper.java
+++ src/main/java/cn/chen/csmall/product/mapper/SpuDetailMapper.java
@@ -1,9 +1,21 @@
 package cn.chen.csmall.product.mapper;
 
 import cn.chen.csmall.product.pojo.entity.SpuDetail;
+import cn.chen.csmall.product.pojo.vo.SpuDetailListItemVO;
+import cn.chen.csmall.product.pojo.vo.SpuDetailStandardVO;
+import cn.chen.csmall.product.pojo.vo.SpuListItemVO;
+import cn.chen.csmall.product.pojo.vo.SpuStandardVO;
+
+import java.util.List;
 
 public interface SpuDetailMapper {
     int insert(SpuDetail spuDetail);
 
     int deleteById(long id);
+
+    int count();
+
+    SpuDetailStandardVO getStandardById(Long id);
+
+    List<SpuDetailListItemVO> list();
 }
---
+++ src/main/java/cn/chen/csmall/product/pojo/vo/SpuDetailListItemVO.java
@@ -0,0 +1,23 @@
+package cn.chen.csmall.product.pojo.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class SpuDetailListItemVO implements Serializable {
+    /**
+     * 数据id
+     */
+    private Long id;
+
+    /**
+     * SPU id
+     */
+    private Long spuId;
+
+    /**
+     * SPU详情，应该使用HTML富文本，通常内容是若干张图片
+     */
+    private String detail;
+}
