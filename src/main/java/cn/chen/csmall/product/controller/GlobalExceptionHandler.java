7f772edfd812276c8e2e82968a717e3308172800
chen <602042954@qq.com>
2023-03-22 17:58:23 +0800

    HandlerExcepiton

--- src/main/java/cn/chen/csmall/product/controller/GlobalExceptionHandler.java
+++ src/main/java/cn/chen/csmall/product/controller/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package cn.chen.csmall.product.controller;
 
 import cn.chen.csmall.product.ex.ServiceException;
 import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * 全局异常处理器
@@ -19,8 +23,17 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public String handleBindException(BindException e) {
-        String message = e.getFieldError().getDefaultMessage();
-        return message;
+//        // 使用以下方式时，如果有多个检查错误，
+//        // 将获取其中某1个检查注解中配置的message值，但无法确定到底是哪一个
+//        String message = e.getFieldError().getDefaultMessage();
+//        return message;
+
+        StringJoiner stringJoiner = new StringJoiner(",", "请求参数错误的是: ", "! ");
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        for (FieldError fieldError : fieldErrors){
+            stringJoiner.add(fieldError.getDefaultMessage());
+        }
+        return stringJoiner.toString();
     }
 
     @ExceptionHandler
--- src/main/java/cn/chen/csmall/product/pojo/dto/AlbumAddNewDTO.java
+++ src/main/java/cn/chen/csmall/product/pojo/dto/AlbumAddNewDTO.java
@@ -12,20 +12,20 @@ public class AlbumAddNewDTO implements Serializable {
      * 相册名称
      */
     @ApiModelProperty(value = "相册名称", required = true, example = "格力空调的名字")
-    @NotNull(message = "添加相册失败，必须提交相册名称！")
+    @NotNull(message = "必须提交相册名称")
     private String name;
 
     /**
      * 相册简介
      */
     @ApiModelProperty(value = "相册简介", required = true)
-    @NotNull(message = "添加相册失败，必须提交相册简介！")
+    @NotNull(message = "必须提交相册简介")
     private String description;
 
     /**
      * 排序序号
      */
     @ApiModelProperty(value = "排序序号", required = true, example = "99")
-    @NotNull(message = "添加相册失败，必须提交相册类型！")
+    @NotNull(message = "必须提交相册类型")
     private Integer sort;
 }
