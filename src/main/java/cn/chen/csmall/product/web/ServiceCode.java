c47d2ed61bbccffaa11fd336e86e616b642c7a27
chen <602042954@qq.com>
2023-03-29 15:03:26 +0800

    getStandardById

--- src/main/java/cn/chen/csmall/product/web/ServiceCode.java
+++ src/main/java/cn/chen/csmall/product/web/ServiceCode.java
@@ -1,23 +1,42 @@
 package cn.chen.csmall.product.web;
 
 /**
- * 枚举
+ * 业务状态码枚举类型
+ *
+ * @author java@tedu.cn
+ * @version 0.0.1
  */
 public enum ServiceCode {
 
+    /**
+     * 成功
+     */
     OK(20000),
+    /**
+     * 错误：请求参数格式有误
+     */
     ERR_BAD_REQUEST(40000),
+    /**
+     * 错误：数据不存在
+     */
     ERR_NOT_FOUND(40400),
-    ERR_UNKNOWN(99999),
-    ERR_CONFLICT(40900);
+    /**
+     * 错误：数据冲突
+     */
+    ERR_CONFLICT(40900),
+    /**
+     * 错误：未知错误
+     */
+    ERR_UNKNOWN(99999);
 
     private Integer value;
 
-    ServiceCode(Integer value){
+    ServiceCode(Integer value) {
         this.value = value;
     }
 
-    public Integer getValue(){
+    public Integer getValue() {
         return value;
     }
+
 }
